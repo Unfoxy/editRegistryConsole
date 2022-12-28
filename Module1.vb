9095bc5f81d5fdee940b8b79b83e94991784ffb3
Unfoxy <114528590+Unfoxy@users.noreply.github.com>
2022-12-28 16:53:02 -0600

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,27 @@
+﻿Imports Microsoft.Win32
+
+Module Module1
+    Sub Main()
+        Console.WriteLine("This program will disable 'IPChecksumOffloadIPv4' in Registry. Make sure run this program as Administrator" & vbCrLf & vbCrLf & "Enter Y to proceed / Enter N to exit")
+        Dim info = Console.ReadLine()
+
+        If info.ToUpper = "Y" Then
+            Dim inputRegistry As RegistryKey
+
+            'local PC
+            inputRegistry = Registry.LocalMachine.OpenSubKey("SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\0001", True)
+
+            'Below 2 for remote pc. 'Remote Registry' in Services.msc has to be enabled.
+            'inputRegistry = RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, "AMMVWCZD81T3-L")
+            'inputRegistry = inputRegistry.OpenSubKey("SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\0001", True)
+            inputRegistry.SetValue("*IPChecksumOffloadIPv4", "0")
+            Console.WriteLine("IPChecksumOffloadIPv4 is disabled. Press any key to exit")
+            Console.ReadKey(True)
+        ElseIf info.ToUpper = "N" Then
+            Exit Sub
+        Else
+            Console.WriteLine("Invalid Entry. Press any key to exit")
+            Console.ReadKey(True)
+        End If
+    End Sub
+End Module
